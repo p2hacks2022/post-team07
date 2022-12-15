03ffd5cfddce4448a784f094f95e3332fe61b0fc
RinoSue <100293563+RinoSue@users.noreply.github.com>
2022-12-15 22:09:37 +0900

    [feat]始めに手札にランダムに選ばれたカードを3枚用意する処理を実装した

--- KamisamaIntern/Assets/Suebayashi_Folder/CardOption/DeckController.cs
+++ KamisamaIntern/Assets/Suebayashi_Folder/CardOption/DeckController.cs
@@ -64,7 +64,35 @@ public class DeckController : MonoBehaviour
         // カードの効果をランダムに選ぶ
         int deckTypeNum = Random.Range(0,5);  
 
-        if ((nowCardNum+1) <= deckCards.Count)
+        if(nowCardNum == 0)
+        {
+            for(int i=0; i<3; i++)
+            {
+                // カードの効果をランダムに選ぶ
+                deckTypeNum = Random.Range(0, 5);
+
+                // 選んだカードの効果画像を設定する
+                deckCards[i].sprite = deckType[deckTypeNum];
+            }
+
+            //deckカードをカード3の子要素にする
+            deckCards[0].transform.SetParent(card3Tf);
+            //カードのlocalPositionを0にする
+            deckCards[0].transform.localPosition = Vector2.zero;
+
+            //deckカードをカード2の子要素にする
+            deckCards[1].transform.SetParent(card2Tf);
+            //カードのlocalPositionを0にする
+            deckCards[1].transform.localPosition = Vector2.zero;
+
+            //deckカードをカード1の子要素にする
+            deckCards[2].transform.SetParent(card1Tf);
+            //カードのlocalPositionを0にする
+            deckCards[2].transform.localPosition = Vector2.zero;
+
+            nowCardNum++;
+        }
+        else if ((nowCardNum+1) <= deckCards.Count)
         {
             // 選んだカードの効果画像を設定する
             deckCards[nowCardNum].sprite = deckType[deckTypeNum];
